bfaa4dd94a3e337a4719953e90d9cffff9baacd3
Andrey Medvedev <betahegra@gmail.com>
2023-02-07 12:08:23 +0300

    Первое задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,85 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < arr.length; i++) {
+      if (max < arr[i]) {
+        max = arr[i];
+      };
+      if (min > arr[i]) {
+        min = arr[i];
+      };
+      sum += arr[i];
+  };
+  avg = sum / arr.length;
+  return { min: min, max: max, avg: Number(avg.toFixed(2)) };
 }
 
 function summElementsWorker(...arr) {
-
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    };
+    return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  let min = Infinity;
+  let max = -Infinity;
+  let difference = 0;
+  if (arr.length === 0) {
+    return 0;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    };
+    if (min > arr[i]) {
+      min = arr[i];
+    };
+  };
+  return difference = max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  let difference = 0;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    }
+    if (arr[i] % 2 != 0) {
+      sumOddElement += arr[i];
+    };
+  };
+  return difference = sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  sumEvenElement = 0;
+  countEvenElement = 0;
+  result = 0;
+  if (arr.length === 0) {
+    return 0;
+    }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      countEvenElement++;
+      sumEvenElement += arr[i];
+    };
+  };
+  return result = sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
-
+  maxWorkerResult = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const result = func(...arrOfArr[i])
+    if (result > maxWorkerResult) {
+      maxWorkerResult = result;
+   }
+  }
+  return maxWorkerResult;
 }
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,30 @@
 function compareArrays(arr1, arr2) {
-  
+    let result = true;
+    if(arr1.length !== arr2.length) {
+        result = false;
+    } else {
+        result = arr1.every((item, index) => item === arr2[index]);
+    }
+//     if(arr1.length !== arr2.length) {
+//         result = false;
+//     } else {
+//     for (let i = 0; i < arr1.length; i++) {
+//         if (arr1[i] !== arr2[i]) {
+//             result = false;
+//             break;
+//         }
+//     }
+// }
+    return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const result =  users.filter(elem => elem.gender === gender).map(elem => elem.age).reduce((acc, item, index, arr) => {
+        if(index === arr.length-1) {
+            return (acc + item) / arr.length;
+   } else {
+       return acc + item;
+   }
+ } ,0)
+    return result;
 }
